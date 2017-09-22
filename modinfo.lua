89b9dc6b6b085a2f9951fc3fb08cdd70ff67f087
folknor <folk@folk.wtf>
2017-09-22 13:06:00 +0200

    Bump to 1.8.0.

--- modinfo.lua
+++ modinfo.lua
@@ -4,7 +4,7 @@ return {
 	name = "Tiny Ship Components",
 	tags = { "Graphics", "Utilities" },
 	picture = "thumb.png",
-	supported_version = "1.6.*",
+	supported_version = "1.8.*",
 	remote_file_id = 804946681,
 	readme = "readme.md",
 	steambb = "steam.bbcode",
